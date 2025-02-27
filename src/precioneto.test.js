8d780cdcc47bce094433f214fcc612b1a2a8ac0c
unknown <nicole.gomez@ucb.edu.bo>
2025-02-27 09:57:56 -0400

    feat: Se añadio un boton de Precio NETO MEJORADO

--- src/precioneto.test.js
+++ src/precioneto.test.js
@@ -2,6 +2,6 @@ import precioneto from "./precioneto.js";
 
 describe("Precio neto de  los Items", () => {
   it("deberia de mostrar el precio neto de los items", () => {
-    expect(num(3, 2)).toEqual(6);
+    expect(precioneto(3, 2)).toEqual(6);
   });
 });
