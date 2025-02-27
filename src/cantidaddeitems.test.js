96d62cd4168d3bcde245c7f164ad5ed1b018fee9
unknown <nicole.gomez@ucb.edu.bo>
2025-02-27 09:48:29 -0400

    feat: Se añadio un boton de Precio

---
+++ src/cantidaddeitems.test.js
@@ -0,0 +1,7 @@
+import cantidaddeitems from "./cantidaddeitems.js";
+
+describe("Cantidad de Items", () => {
+  it("deberia de mostrar la cantida de item", () => {
+    expect(num(3, 2)).toEqual(5);
+  });
+});
