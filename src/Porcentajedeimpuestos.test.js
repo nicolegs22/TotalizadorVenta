35e19edf774c3eaead36a617d859e942880928cf
JaimeTadeo <deugartebutron@gmail.com>
2025-03-07 19:28:34 -0400

    feat:andido porcentaje de iompuestos de ca

--- src/Porcentajedeimpuestos.test.js
+++ src/Porcentajedeimpuestos.test.js
@@ -1,20 +1,5 @@
 import porcentajedeimpuesto from "./porcentajedeimpuestos";
 
-test("Calcula impuesto para UT (6.65%)", () => {
-  expect(porcentajedeimpuesto(100, "UT")).toBeCloseTo(6.65);
-});
-
-test("Calcula impuesto para NV (8.00%)", () => {
-  expect(porcentajedeimpuesto(100, "NV")).toBeCloseTo(8.00);
-});
-
-test("Calcula impuesto para TX (6.25%)", () => {
-  expect(porcentajedeimpuesto(100, "TX")).toBeCloseTo(6.25);
-});
-
-test("Calcula impuesto para AL (4.00%)", () => {
-  expect(porcentajedeimpuesto(100, "AL")).toBeCloseTo(4.00);
-});
 
 test("Calcula impuesto para CA (8.25%)", () => {
   expect(porcentajedeimpuesto(100, "CA")).toBeCloseTo(8.25);
--- src/porcentajedeimpuestos.js
+++ src/porcentajedeimpuestos.js
@@ -1,17 +1,12 @@
 const tasasDeImpuestos = {
-  UT: 6.65,
-  NV: 8.00,
-  TX: 6.25,
-  AL: 4.00,
   CA: 8.25
 };
-
-function porcentajedeimpuesto(monto, estado) {
-  const porcentaje = tasasDeImpuestos[estado];
-  if (porcentaje === undefined) {
-    throw new Error("Estado no válido");
+  function porcentajedeimpuesto(monto, estado) {
+    const porcentaje = tasasDeImpuestos[estado];
+    if (porcentaje === undefined) {
+      throw new Error("Estado no válido");
+    }
+    return monto * (porcentaje / 100);
   }
-  return monto * (porcentaje / 100);
-}
-
-export default porcentajedeimpuesto;
+  
+  export default porcentajedeimpuesto;
