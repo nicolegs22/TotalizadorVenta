581f82e2a0f5da43174e4867967f367d2009457d
JaimeTadeo <deugartebutron@gmail.com>
2025-03-07 22:20:03 -0400

    feat:agregado calcular precio con categoria de Muebles

--- src/categoria.js
+++ src/categoria.js
@@ -14,6 +14,21 @@ function calcularPrecioConCategoria(totalNeto, impuesto, categoria) {
         return parseFloat(totalConImpuesto.toFixed(2));
     }
 
+    if (categoria === "Material de escritorio") {
+        const impuestoAdicional = 0;
+        const descuentoAdicional = 1.5;
+        const totalConImpuesto = totalNeto + impuesto;
+        const descuento = (totalConImpuesto * descuentoAdicional) / 100;
+        return parseFloat((totalConImpuesto - descuento).toFixed(2));
+    }
+
+    if (categoria === "Muebles") {
+        const impuestoAdicional = 3;
+        const descuentoAdicional = 0;
+        const totalConImpuesto = totalNeto + impuesto + (totalNeto * impuestoAdicional) / 100;
+        return parseFloat(totalConImpuesto.toFixed(2));
+    }
+
     return totalNeto + impuesto;  
 }
 
--- src/categoria.test.js
+++ src/categoria.test.js
@@ -8,4 +8,12 @@ describe("Cálculo de precio con categoría de producto", () => {
     it("debería aplicar el impuesto para la categoría Bebidas alcohólicas (sin descuento)", () => {
         expect(calcularPrecioConCategoria(1000, 100, "Bebidas alcohólicas")).toBe(1170.00);
     });
+
+    it("debería aplicar el descuento para la categoría Material de escritorio (sin impuesto adicional)", () => {
+        expect(calcularPrecioConCategoria(1000, 100, "Material de escritorio")).toBe(1083.5);
+    });
+
+    it("debería aplicar el impuesto adicional para la categoría Muebles", () => {
+        expect(calcularPrecioConCategoria(1000, 100, "Muebles")).toBe(1130.00);
+    });
 });
